e0295507fa888e168e5133389bdc5afa4cb99cf0
islamaboalkhair <157137806+islamaboalkhair@users.noreply.github.com>
2024-03-17 22:08:49 +0200

    Update main.go

--- main.go
+++ main.go
@@ -28,8 +28,7 @@ import (
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
-	clientset "k8s.io/sample-controller/pkg/generated/clientset/versioned"
-	informers "k8s.io/sample-controller/pkg/generated/informers/externalversions"
+
 )
 
 var (
